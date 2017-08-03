d821d088de316881140583f307bfe11b106df298
Blake Lockley <bwlockley@gmail.com>
2017-08-03 22:28:01 +1000

    Initial Commit

---
+++ ContactsiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ContactsiOS
+//
+//  Created by Blake Lockley on 3/8/17.
+//  Copyright © 2017 Blake Lockley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
